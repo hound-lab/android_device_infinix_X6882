d3aa4747880a23ea666d23e5932260e004e0a06a
LuK1337 <priv.luk@gmail.com>
2026-09-19 06:19:04 +0700

    X6882: Define AAPT and density screen
    Change-Id: I3811d5d8da0b338f41f085d6f766be63a8724850
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := 34
